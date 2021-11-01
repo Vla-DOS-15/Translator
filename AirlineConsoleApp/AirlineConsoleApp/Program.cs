c121a1e7613a5952cf81883e01888d8ed0c8ceed
Valentin1993 <Valentin1993@hmail.com>
2021-11-01 05:29:53 +0200

    https://github.com/Vla-DOS-15/Translator.git

---
+++ AirlineConsoleApp/AirlineConsoleApp/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StructFile
+{
+    public struct Airline
+    {
+        public string Destination { get; set; }
+        public string Number { get; set; }
+        public string PlaneModel { get; set; }
+
+        public override string ToString() => $"Destination: {Destination}. Number: {Number}. Plane model: {PlaneModel}";
+    }
+    class Program
+    {
+        const int NUMBER_OF_FLIGHTS = 3;
+        static string Line() => "_______________________________________";
+
+        static void GetValues(Airline[] Flights)
+        {
+            Console.WriteLine(Line());
+
+            for (int i = 0; i < NUMBER_OF_FLIGHTS; i++)
+            {
+                Airline flight = new Airline();
+
+                Console.Write("\n Input destination: ");
+                flight.Destination = Console.ReadLine();
+                Console.Write(" Input flight number: ");
+                flight.Number = Console.ReadLine();
+                Console.Write(" Input plane model: ");
+                flight.PlaneModel = Console.ReadLine();
+                Flights[i] = flight;
+            }
+            Console.WriteLine(Line());
+        }
+
+        static void WriteTxt(string pathTxt, Airline[] Flights)
+        {
+            using (StreamWriter sw = new StreamWriter(pathTxt, false, Encoding.UTF8))
+            {
+                for (int i = 0; i < NUMBER_OF_FLIGHTS; i++)
+                    sw.WriteLine(Flights[i]);
+            }
+        }
+
+        static void ReadTxt(string pathTxt)
+        {
+            using (StreamReader sr = new StreamReader(pathTxt, Encoding.UTF8))
+                Console.WriteLine(sr.ReadToEnd());
+
+            Console.WriteLine(Line());
+        }
+        static void WriteXml(string pathXml, Airline[] Flights, XmlSerializer xmlSerialaizer)
+        {
+            List<Airline> list = new List<Airline>();
+            list.AddRange(Flights);
+
+            using (FileStream fw = new FileStream(pathXml, FileMode.OpenOrCreate))
+                xmlSerialaizer.Serialize(fw, list);
+        }
+        static void ReadXml(string pathXml, XmlSerializer xmlSerialaizer)
+        {
+            List<Airline> FlightsToRead = new List<Airline>();
+
+            using (FileStream fr = new FileStream(pathXml, FileMode.Open))
+                FlightsToRead = (List<Airline>)xmlSerialaizer.Deserialize(fr);
+
+            foreach (Airline i in FlightsToRead)
+                Console.WriteLine($" Destination: {i.Destination}. Number: {i.Number}. Plane model: {i.PlaneModel}");
+
+            Console.WriteLine(Line());
+        }
+
+        static void Sort(Airline[] Flights)
+        {
+            var sort = Flights.OrderBy(aeroflot => aeroflot.Number);
+
+            Console.WriteLine(" SORTED IN ASCENDING ORDER:\n");
+            foreach (var s in sort)
+                Console.WriteLine(s);
+
+            Console.WriteLine(Line());
+        }
+
+        static void CheckFlight(Airline[] Flights)
+        {
+            Console.Write("\n Input destination: ");
+
+            string DestinationToCompare = Console.ReadLine();
+            int matches = 0;
+
+            for (int i = 0; i < Flights.Length; i++)
+            {
+                if (Flights[i].Destination == DestinationToCompare)
+                {
+                    Console.WriteLine($" Number: {Flights[i].Number}. Plane Model: {Flights[i].PlaneModel}");
+                    matches++;
+                }
+            }
+            if (matches == 0)
+            {
+                Console.WriteLine("\n THERE IS NO MATCHES!!");
+            }
+            Console.WriteLine(Line());
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Airline[] Flights = new Airline[NUMBER_OF_FLIGHTS];
+
+                Console.WriteLine(" INPUT THE DATA:");
+
+                GetValues(Flights);
+
+                string pathTxt = "file1.txt";
+                WriteTxt(pathTxt, Flights);
+
+                Console.WriteLine(" READ FILE .txt:\n");
+                ReadTxt(pathTxt);
+
+                XmlSerializer xmlSerialaizer = new XmlSerializer(typeof(List<Airline>));
+
+                string pathXml = "file2.xml";
+                WriteXml(pathXml, Flights, xmlSerialaizer);
+
+                Console.WriteLine(" READ FILE .xml:\n");
+                ReadXml(pathXml, xmlSerialaizer);
+
+                Sort(Flights);
+
+                CheckFlight(Flights);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleAppTranslator/ConsoleAppTranslator/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Data;
+
+namespace ConsoleAppTranslator
+{
+    class Program
+    {
+        static void WriteXml(string pathXml, List<Word> words, XmlSerializer xmlSerialaizer)
+        {
+            List<Word> list = new List<Word>();
+            list.AddRange(words);
+
+            using (FileStream fw = new FileStream(pathXml, FileMode.OpenOrCreate))
+                xmlSerialaizer.Serialize(fw, list);
+        }
+        static void ReadXml(string pathXml, XmlSerializer xmlSerialaizer, string WordValue)
+        {
+            List<Word> WordsToRead = new List<Word>();
+
+            using (FileStream fr = new FileStream(pathXml, FileMode.Open))
+                WordsToRead = (List<Word>)xmlSerialaizer.Deserialize(fr);
+
+            foreach (Word i in WordsToRead)
+            {
+                if (i.Russian == WordValue)
+                    Console.WriteLine($"{i.Russian} - {i.English}");
+            }
+        }
+
+        static void ReadListToKey(List<Word> Words, string key)
+        {
+            foreach (var item in Words)
+            {
+                if (item.Russian == key)
+                    Console.WriteLine(item.English + " - " + item.Russian);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var Words = new List<Word>();
+                Words.Add(new Word() { Russian = "Привет", English = "Hello" });
+                Words.Add(new Word() { Russian = "Мир", English = "World" });
+                Words.Add(new Word() { Russian = "Мир", English = "World" });
+
+                XmlSerializer xmlSerialaizer = new XmlSerializer(typeof(List<Word>));
+
+                string pathXml = "Translate.xml";
+
+                string WordValue = Console.ReadLine();
+
+                WriteXml(pathXml, Words, xmlSerialaizer);
+
+                Console.WriteLine(" READ FILE .xml:\n");
+                ReadXml(pathXml, xmlSerialaizer, WordValue);
+
+                Console.WriteLine("--------");
+                ReadListToKey(Words, WordValue);
+                Console.WriteLine("what type Translate: ");
+                byte count = byte.Parse(Console.ReadLine());
+
+                do
+                {
+                    switch (count)
+                    {
+                        case 1:
+                            {
+                                string key = Console.ReadLine();
+                                foreach (var item in Words)
+                                {
+                                    if (item.Russian == key)
+                                        Console.WriteLine(item.English);
+                                }
+                            }
+                            break;
+                        case 2:
+                            {
+                                string key = Console.ReadLine();
+                                foreach (var item in Words)
+                                {
+                                    if (item.English == key)
+                                        Console.WriteLine(item.Russian);
+                                }
+                            }
+                            break;
+                        case 3:
+                            {
+                                Console.WriteLine("Add rus: ");
+                                string AddWordRus = Console.ReadLine();
+                                Console.WriteLine("Add eng: ");
+                                string AddWordEng = Console.ReadLine();
+
+                                Words.Add(new Word() { Russian = AddWordRus, English = AddWordEng });
+                                WriteXml(pathXml, Words, xmlSerialaizer);
+                                Console.WriteLine(" READ FILE .xml:\n");
+                                 ReadXml(pathXml, xmlSerialaizer, WordValue);
+                            }
+                            break;
+                        case 4:
+                            {
+
+                            }
+                            break;
+                    }                 
+
+                } while (count != 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
